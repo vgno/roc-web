93e5f90d64014278086b0c86056f9ce576c12033
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2015-11-10 13:29:21 +0100

    Moved examples into the project

---
+++ examples/client-server/roc.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    port: 8080,
+    serve: 'files',
+    favicon: 'files/roc.png',
+    build: {
+        entry: {
+            client: 'client.js',
+            server: 'server.js'
+        }
+    },
+    dev: {
+        open: true
+    }
+};
---
+++ examples/server/index.js
@@ -0,0 +1,5 @@
+import { createServer } from 'roc-web/app';
+
+createServer({
+    serve: 'files'
+}).start();
