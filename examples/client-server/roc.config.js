7dbd3674b39d9d1ce1dd2ba8c0e0d6bf99efb91e
Andreas Søvik <andreas.sovik@vg.no>
2015-11-12 14:51:23 +0100

    Fix example configuration bug

--- examples/client-server/roc.config.js
+++ examples/client-server/roc.config.js
@@ -1,14 +1,16 @@
 module.exports = {
-    port: 8080,
-    serve: 'files',
-    favicon: 'files/roc.png',
-    build: {
-        entry: {
-            client: 'client.js',
-            server: 'server.js'
+    config: {
+        port: 8080,
+        serve: 'files',
+        favicon: 'files/roc.png',
+        build: {
+            entry: {
+                client: 'client.js',
+                server: 'server.js'
+            }
+        },
+        dev: {
+            open: true
         }
-    },
-    dev: {
-        open: true
     }
 };
