46e90cc630f2d9e6f7f6fc8666a496db0f28f260
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2016-01-25 15:23:50 +0100

    Added punctuation to the descriptions

--- src/roc/config/roc.config.meta.js
+++ src/roc/config/roc.config.meta.js
@@ -13,75 +13,75 @@ const configMeta = {
     settings: {
         groups: {
             runtime: {
-                koa: 'Settings for how Koa should handle paths'
+                koa: 'Settings for how Koa should handle paths.'
             }
         },
         descriptions: {
             runtime: {
-                port: 'Port for the server to use',
+                port: 'Port for the server to use.',
                 debug: {
                     server: 'Filter for debug messages that should be shown for the server, see ' +
-                        'https://npmjs.com/package/debug'
+                        'https://npmjs.com/package/debug.'
                 },
-                serve: 'What folder the server should expose',
-                favicon: 'Path to the favicon file, specially handled on the server',
-                startBundle: 'Relative path to a bundle to start when calling "start", is not needed in most cases',
+                serve: 'What folder the server should expose.',
+                favicon: 'Path to the favicon file, specially handled on the server.',
+                startBundle: 'Relative path to a bundle to start when calling "start", is not needed in most cases.',
                 koa: {
                     lowercase: {
-                        enabled: 'If paths should be transformed to lowercase',
-                        defer: 'If this should be performed after looking for a file on disk'
+                        enabled: 'If paths should be transformed to lowercase.',
+                        defer: 'If this should be performed after looking for a file on disk.'
                     },
                     normalize: {
-                        enabled: 'If paths should be normalized, that is remove extra slashes',
-                        defer: 'If this should be performed after looking for a file on disk'
+                        enabled: 'If paths should be normalized, that is remove extra slashes.',
+                        defer: 'If this should be performed after looking for a file on disk.'
                     },
                     trailingSlashes: {
-                        enabled: 'Set to true to enforce trailing slashes, false to remove them and null for no rule',
-                        defer: 'If this should be performed after looking for a file on disk'
+                        enabled: 'Set to true to enforce trailing slashes, false to remove them and null for no rule.',
+                        defer: 'If this should be performed after looking for a file on disk.'
                     }
                 }
             },
 
             dev: {
                 debug: 'Filter for debug messages that should be shown for the server, see ' +
-                    'https://npmjs.com/package/debug',
-                port: 'Port for the dev server, will need to be a free range of at least 3',
-                watch: 'Files/folders that should trigger a restart of the server',
-                reloadOnServerChange: 'If Browsersync should reload the browser when the server is rebuilt',
-                open: 'If Browsersync should open the server when it has started',
+                    'https://npmjs.com/package/debug.',
+                port: 'Port for the dev server, will need to be a free range of at least 3.',
+                watch: 'Files/folders that should trigger a restart of the server.',
+                reloadOnServerChange: 'If Browsersync should reload the browser when the server is rebuilt.',
+                open: 'If Browsersync should open the server when it has started.',
                 devMiddleware: {
-                    noInfo: 'If no info should be sent to the console',
-                    quiet: 'If nothing should be sent to the console'
+                    noInfo: 'If no info should be sent to the console.',
+                    quiet: 'If nothing should be sent to the console.'
                 },
                 hotMiddleware: {
-                    reload: 'If the browser should be reloaded if it fails to hot update the code',
-                    noInfo: 'If no info should be sent to the console',
-                    quiet: 'If nothing should be sent to the console'
+                    reload: 'If the browser should be reloaded if it fails to hot update the code.',
+                    noInfo: 'If no info should be sent to the console.',
+                    quiet: 'If nothing should be sent to the console.'
                 }
             },
 
             build: {
-                path: 'The basepath for the application',
-                assets: 'An array of files to include into the build process',
-                verbose: 'If verbose mode should be used, returns more output during build',
-                mode: 'What mode the application should be built for. Possible values are "dev", "dist" and "test"',
-                target: 'For what target the application should be built for. Possible values are "client" & "server"',
-                disableProgressbar: 'Should the progress bar be disabled for builds',
+                path: 'The basepath for the application.',
+                assets: 'An array of files to include into the build process.',
+                verbose: 'If verbose mode should be used, returns more output during build.',
+                mode: 'What mode the application should be built for. Possible values are "dev", "dist" and "test".',
+                target: 'For what target the application should be built for. Possible values are "client" & "server".',
+                disableProgressbar: 'Should the progress bar be disabled for builds.',
                 entry: {
-                    client: 'The client entry point file',
-                    server: 'The server entry point file'
+                    client: 'The client entry point file.',
+                    server: 'The server entry point file.'
                 },
-                outputName: 'The name of the generated application bundle, will be appended "roc.js"',
+                outputName: 'The name of the generated application bundle, will be appended "roc.js".',
                 outputPath: {
-                    client: 'The output directory for the client build',
-                    server: 'The output directory for the server build'
+                    client: 'The output directory for the client build.',
+                    server: 'The output directory for the server build.'
                 },
                 moduleBuild: 'NOT IMPLEMENTED YET',
                 moduleStyle: 'NOT IMPLEMENTED YET',
                 koaMiddlewares: 'The koa middlewares to add to the server instance, will be added after the default ' +
-                    ' middlewares',
+                    ' middlewares.',
                 useDefaultKoaMiddlewares: 'If Roc should use internally defined koa middlewares, please look at the ' +
-                    ' documentation for what middlewares that are included'
+                    ' documentation for what middlewares that are included.'
             }
         },
 
